f4cd22d6561e19ac5e2630ba7cbf4617e06ee257
912-Cristea-Aida <aida.cristea@stud.ubbcluj.ro>
2023-12-28 18:17:22 +0200

    Backup  correct version

--- Lab5Parser/src/Main.java
+++ Lab5Parser/src/Main.java
@@ -14,6 +14,7 @@ public class Main {
         FileCleaner fileCleaner = new FileCleaner();
         fileCleaner.cleaner("PIF1.out");
         fileCleaner.cleaner("PIFSeq.out");
+        fileCleaner.cleaner("ST1.out");
 
 
         PIF pif = new PIF();
